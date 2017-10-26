fe3dae774e7fed0d9312f8888dea9c4520666769
hasibulislam66 <hasibulislamcse@gmail.com>
2017-10-26 23:27:59 +0600

    1886 no Solution Added

---
+++ src/Solution_1866_Bill/Main.java
@@ -0,0 +1,32 @@
+package Solution_1866_Bill;
+/*
+ * Hasibul Islam
+ * Read Problem attetively.
+ * Very Simple Problem.
+ */
+import java.util.Scanner;
+
+class Bill{
+	int n;
+	int gnrts(){ //generate Summation of Series.
+		int s=1;	
+		if (n%2==0){
+			s = 0;
+		}
+		return s;
+	}
+}
+
+public class Main {
+	public static void main (String[] args){
+		Scanner sc = new Scanner(System.in);
+		Bill bl = new Bill();
+		int t, i;
+		t = sc.nextInt();
+		
+		for (i=0; i<t; i++){
+			bl.n = sc.nextInt();
+			System.out.println(bl.gnrts());
+		}
+	}
+}
---
+++ src/Solution_1866_Bill/Solution3.java
@@ -0,0 +1,42 @@
+package Solution_1866_Bill;
+
+import java.util.Scanner;
+/*
+ * Author:~ Hasibul Islam
+ * package keyword is not allowed to URI
+ */
+
+class Bill3{
+	int n;
+	int gnrts(){
+		int i, sum=0, flag=0;
+		for (i=1; i<=n; i++){
+			if (flag==0){
+				sum += 1;
+				flag = 1;
+			}
+			else {
+				sum -= 1;
+				flag = 0;
+			}
+		}
+		return sum;
+	}
+}
+
+public class Solution3 {
+	/*
+	 * to submit, you must define class main as Main
+	 */
+	private static Scanner sc;
+	public static void main (String[] args){
+		sc = new Scanner(System.in);
+		Bill3 bl = new Bill3();
+		int t, i;
+		t = sc.nextInt();
+		for (i=0; i<t; i++){
+			bl.n = sc.nextInt();
+			System.out.println(bl.gnrts());
+		}
+	}
+}
