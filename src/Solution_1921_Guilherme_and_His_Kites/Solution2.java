7eca2ffe9b854d07e9c554db13a09193dd05b112
hasibulislam66 <hasibulislamcse@gmail.com>
2017-10-27 22:23:37 +0600

    1921 no Solution Updated

---
+++ src/Solution_1921_Guilherme_and_His_Kites/Solution2.java
@@ -0,0 +1,25 @@
+package Solution_1921_Guilherme_and_His_Kites;
+
+import java.util.Scanner;
+
+class GuilhermeandKites2{
+	long determinestable(long m){
+		m = (m*(m-3))/2;
+		/*m = (m*(m-3))/2;
+		 *or,
+		 *m = (m*(m+1)/2)-2*m;
+		 */
+		return m;
+	}
+}
+
+public class Solution2 {
+	private static Scanner sc;
+	private static GuilhermeandKites2 gk;
+	public static void main (String[] args){
+		sc = new Scanner(System.in);
+		gk = new GuilhermeandKites2();
+		long n = sc.nextInt();
+		System.out.println(gk.determinestable(n));
+	}
+}
