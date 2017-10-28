9231bd101d5fd36fe0fecfd07ff27e61b1b88635
hasibulislam66 <hasibulislamcse@gmail.com>
2017-10-28 23:32:48 +0600

    1924 no solution Updated

---
+++ src/Solution_1924_Victoria/Main.java
@@ -0,0 +1,25 @@
+package Solution_1924_Victoria;
+
+import java.util.Scanner;
+
+class Victoria{
+	char str[];
+	void course(){
+		System.out.println("Ciencia da Computacao");
+	}
+}
+
+public class Main {
+	private static Scanner sc;
+	public static void main(String[] args){
+		sc = new Scanner(System.in);
+		Victoria vctra = new Victoria();
+		int t, i;
+		
+		t = sc.nextInt();
+		for (i=1; i<=t; i++){
+			vctra.str = sc.nextLine().toCharArray();
+		}
+		vctra.course();
+	}
+}
