79827947c1c41f65018c9a37ffa07be62f908dcf
hasibul <hasibul@localhost.localdomain>
2017-11-05 10:14:23 +0600

    Problem no 1959's Solution Updated

---
+++ src/Solution_1959/Main.java
@@ -0,0 +1,22 @@
+package Solution_1959;
+
+import java.util.Scanner;
+
+class RegularSimplePolygons{
+	long n;
+	int l;
+	long Polygons(){
+		return l*n;
+	}
+}
+
+public class Main {
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		RegularSimplePolygons rsp = new RegularSimplePolygons();
+		
+		rsp.n = sc.nextLong();
+		rsp.l = sc.nextInt();
+		System.out.println(rsp.Polygons());
+	}
+}
