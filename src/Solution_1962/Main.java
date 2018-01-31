34b8f95975a8bed02010963e7af0684a48408b13
hasibul-islam-niaj <hasibulislamcse@gmail.com>
2018-02-01 00:41:25 +0600

    1962 no solution updated

---
+++ src/Solution_1962/Main.java
@@ -0,0 +1,34 @@
+package Solution_1962;
+
+import java.util.Scanner;
+
+class ALongTimeAgo{
+	int year, testCase;
+	
+	int HappenedBC_AD(int year) {
+		if (year < 2015)
+			System.out.println(2015-year + " D.C.");
+		else 
+			System.out.println(year-2014 + " A.C.");
+		return 0;
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		ALongTimeAgo alta = new ALongTimeAgo();
+		Scanner sc = new Scanner (System.in);
+		
+		int i=1;
+		
+		alta.testCase = sc.nextInt();
+		
+		while (i<=alta.testCase) {
+			alta.year = sc.nextInt();
+			alta.HappenedBC_AD(alta.year);
+			i++;
+		}
+		
+		System.exit(0);
+	}
+}
